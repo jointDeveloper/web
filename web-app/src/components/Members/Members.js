1b7039278807b384e1391d8bc82ad3f85d0f127c
Carolina <carolina.jimenez.g@gmail.com>
2019-01-10 23:54:26 -0500

    Finished members section

--- web-app/src/components/Members/Members.js
+++ web-app/src/components/Members/Members.js
@@ -6,26 +6,51 @@ class Members extends Component {
   constructor() {
     super();
     this.state = {
-      members
+      members,
+      userPicture: "images/members/user.png"
     }
   }
 
   render() {
-    const members = this.state.members.map((member, index) => {
-      let id = "member" + index.toString();
+    let members = this.state.members.map((member, index) => {
       return (
-        <div>
-          <p key={id + "name"} className="font-subtitle">{member.name}</p>
-          <p key={id + "pic"} className="">{member.picture}</p>
-          <p key={id + "quote"} className="font-quote">"This is a quote"</p>
-          <p key={id + "highlighted"} className="font-highlighted-paragraph">{"This is another paragraph"}</p>
+        <div className="col" key={index + "member"}>
+          <img className="member-pic rounded-circle img-responsive" src={ member.picture != "" ? member.picture : this.state.userPicture } />
+          <p className="member-name text-white font-weight-bold text-center">{member.name}</p>
+          <p className="member-info text-white text-center">{member.info}</p>
+          <div className="row d-flex justify-content-center">
+            { member.linkedin != "" ? (<a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-white" id="linkedin">
+                                        <i className="fab fa-linkedin" aria-hidden="true"></i>
+                                       </a>) : null }
+            { member.github != "" ? (<a href={member.github} target="_blank" rel="noopener noreferrer" className="text-white">
+                                       <i className="fab fa-github-alt" aria-hidden="true"></i>
+                                     </a>) : null }
+          </div>
         </div>
       );
     });
+
+    let membersAux = [];
+    let max, size = members.length;
+    for (let i = 0; i < size; i += 3) {
+      max = members.length < 3 ? members.length : 3;
+      membersAux.push(members.splice(0, max));
+    }
+
+    const rowsOfMembers = membersAux.map((memberRow, index) => {
+      return (
+        <div className="row member-row" key={index + "memberRow"}>
+          { memberRow }
+        </div>
+      );
+    });
+
     return (
-      <div className="Members">
-        <h1 className="font-title">Miembros</h1>
-        { members }
+      <div className="Members bg-navy-light">
+        <div className="row d-flex justify-content-center">
+          <h1 className="font-title text-white">Miembros</h1>
+        </div>
+        { rowsOfMembers }
       </div>
     );
   }
