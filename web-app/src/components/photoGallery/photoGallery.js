4464738956f57ce7b9f00de70e7c853cd50979fa
Carolina <carolina.jimenez.g@gmail.com>
2019-01-31 16:58:35 -0200

    Add slider in photoGallery

--- web-app/src/components/photoGallery/photoGallery.js
+++ web-app/src/components/photoGallery/photoGallery.js
@@ -8,7 +8,10 @@ class photoGallery extends React.Component {
     super(props);
     this.state = {
       events: [],
-      carouselImages: []
+      carouselImages: [],
+      eventsItems: [],
+      i: 0,
+      j: -1
     }
   }
 
@@ -18,7 +21,7 @@ class photoGallery extends React.Component {
   }
 
   componentWillUnmount() {
-    this.setState({ events: [], carouselImages: [] });
+    this.setState({ events: [], carouselImages: [], eventsItems: [] });
     window.removeEventListener('mousemove', this.onMouseOver);
   }
 
@@ -34,6 +37,25 @@ class photoGallery extends React.Component {
         this.setState({
           events: [...this.state.events, events[i]]
         });
+
+        if (this.state.events.length <= 4) {
+          let item = (
+            <div className="col event-container active" key={"eventItem" + i}>
+                <div className="text-center image-container" onClick={(e) => this.getCarouselImages(e, events[i])}>
+                  <img className="event-pic img-responsive rounded carousel-img" alt="" src={ events[i].imageSrc } />
+                  <div className="text-centered font-weight-bold info-container hide">
+                    <p>{ events[i].title }</p>
+                    <p className="event-date">{ events[i].date }</p>
+                  </div>
+              </div>
+            </div>
+          );
+          this.setState({
+            eventsItems: [...this.state.eventsItems, item],
+            j: this.state.j + 1
+          });
+        }
+
       }).catch((error) => {
         console.error(error);
       });
@@ -79,42 +101,95 @@ class photoGallery extends React.Component {
     }
   }
 
-  showImages = (e, eventItem) => {
+  getCarouselImages = (e, eventItem) => {
     e.preventDefault();
     let images = [];
     images.push(eventItem.images_src + "cover.jpg");
     for (let i = 1; i <= eventItem.items; i++) {
       images.push(eventItem.images_src + i + ".jpg");
     }
-    this.setState({ carouselImages: images });
+    this.updateCarouselImages(images);
+  }
+
+  updateCarouselImages = async (newImages) => {
+    let storage = this.props.firebase.getStorage();
+    await this.setState({ carouselImages: [] });
+
+    for (let i = 0; i < newImages.length; i++) {
+      let pathReference = storage.ref(newImages[i]);
+
+      pathReference.getDownloadURL().then((url) => {
+        this.setState({
+          carouselImages: [...this.state.carouselImages, url]
+        });
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
+  }
+
+  showPrevElement = () => {
+    let size = this.state.events.length;
+    let i = this.state.i;
+    let j = this.state.j;
+    i += 1;
+    j += 1;
+    let new_i = (i > (size - 1) ? 0 : i);
+    let new_j = (j > (size - 1) ? 0 : j);
+    // removes first element of eventsItems and get the next element of events
+    let eventsItems = this.state.eventsItems;
+    eventsItems.shift();
+    eventsItems.push(this.getEventItem(new_j));
+    this.setState({
+      i: new_i,
+      j: new_j,
+      eventsItems
+    });
+  }
+
+  showNextElement = () => {
+    let size = this.state.events.length;
+    let i = this.state.i;
+    let j = this.state.j;
+    i -= 1;
+    j -= 1;
+    let new_i = (i < 0 ? (size - 1) : i);
+    let new_j = (j < 0 ? (size - 1) : j);
+    // removes last element of eventsItems and get a new element from events
+    let eventsItems = this.state.eventsItems;
+    eventsItems.pop();
+    eventsItems.unshift(this.getEventItem(new_i));
+    this.setState({
+      i: new_i,
+      j: new_j,
+      eventsItems
+    });
+  }
+
+  getEventItem = (index) => {
+    let events = this.state.events;
+    let item = (
+      <div className="col event-container active" key={"eventItem" + index}>
+          <div className="text-center image-container" onClick={(e) => this.getCarouselImages(e, events[index])}>
+            <img className="event-pic img-responsive rounded carousel-img" alt="" src={ events[index].imageSrc } />
+            <div className="text-centered font-weight-bold info-container hide">
+              <p>{ events[index].title }</p>
+              <p className="event-date">{ events[index].date }</p>
+            </div>
+        </div>
+      </div>
+    );
+
+    return item;
   }
 
   render() {
-    let eventsItems = this.state.events.map((event, index) => {
+    let imagesItems = this.state.carouselImages.map((image, index) => {
+      let classNameAux = "carousel-item";
+      classNameAux += index === 0 ? " active" : "";
       return (
-        <div className="col event-container" key={"eventsItem" + index}>
-          <div className="text-center image-container" onClick={(e) => this.showImages(e, event)}>
-            <img className="event-pic img-responsive rounded" alt="" src={ event.imageSrc } />
-            <div className="text-centered font-weight-bold info-container hide">
-              <p>{ event.title }</p>
-              <p className="event-date">{ event.date }</p>
-            </div>
-          </div>
-        </div>
-      );
-    });
-
-    let eventsItemsAux = [];
-    let max, size = eventsItems.length;
-    for (let i = 0; i < size; i += 4) {
-      max = eventsItems.length < 4 ? eventsItems.length : 4;
-      eventsItemsAux.push(eventsItems.splice(0, max));
-    }
-
-    const rowsOfEvents = eventsItemsAux.map((eventRow, index) => {
-      return (
-        <div className="row event-row" key={index + "eventRow"}>
-          { eventRow }
+        <div className={ classNameAux } key={index + "carousel"}>
+          <img className="d-block carousel-img" src={ image } alt="" />
         </div>
       );
     });
@@ -122,8 +197,22 @@ class photoGallery extends React.Component {
     return (
       <div className="photoGallery bg-pink-yarrow-gradient">
         <h1 className="font-title text-center text-white">Galería de Fotos</h1>
-        <Carousel images={this.state.carouselImages} firebase={this.props.firebase} />
-        { rowsOfEvents }
+        <Carousel images={ imagesItems }
+                  carouselId="carouselPhotos"
+                  isMultipleCarousel={ false }
+                  showPrevElement={ this.showPrevElement }
+                  showNextElement={ this.showNextElement }
+                  firebase={ this.props.firebase }
+        />
+        <div className="row">
+          <Carousel images={ this.state.eventsItems }
+                    carouselId="carouselEvents"
+                    isMultipleCarousel={ true }
+                    showPrevElement={ this.showPrevElement }
+                    showNextElement={ this.showNextElement }
+                    firebase={ this.props.firebase }
+          />
+        </div>
       </div>
     );
   }
